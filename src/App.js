33469654f10c0c8fe5b2514c16b85218bc941468
Bhob Rainey <lloydlyson@gmail.com>
2021-05-11 14:34:28 -0400

    Setup base code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import Accordion from './components/Accordion';
-// get dummy data
-import items from './data/AccordionItems';
 
 import './App.css';
 
 class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      accordionItems: []
-    }
-  }
-
-  componentDidMount() {
-    this.getAccordionItems();
-  }
-
-  getAccordionItems() {
-    Promise.resolve(items)
-      .then(data => {
-        this.setState({ accordionItems: data })
-      });
-  }
-
   render() { 
     return (
       <div className="App">
         <h2>FAQ</h2>
-        <Accordion items={this.state.accordionItems} />
       </div>
     );
   }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,42 +1,13 @@
 import React from "react";
-import AccordionItem from "./AccordionItem";
 
 import "./Accordion.css";
 
 class Accordion extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: props.items,
-            selectedItem: null
-        }
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.items === this.props.items) return;
-        this.setState({ items: this.props.items });
-    }
-
-    itemChanged(index, isActive) {
-        this.setState({ selectedItem: isActive ? index : null });
-    }
-
     render() {
-        if (!this.state.items || !this.state.items.length) return ''; 
 
         return (
             <section className="accordion__container">
-                {
-                this.state.items.map((item, c) => (
-                    <AccordionItem 
-                        key={c} 
-                        title={item.title} 
-                        content={item.content} 
-                        isActive={!!(c === this.state.selectedItem)} 
-                        onChange={(active) => this.itemChanged(c, active)} />
-                ))
-                }
             </section>
         )
     }
