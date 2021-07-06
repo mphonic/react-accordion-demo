3ff9edf8e1f85e994cf53263ba8705594d7e37b3
Bhob Rainey <lloydlyson@gmail.com>
2021-07-06 11:22:31 -0400

    useState, useEffect, useRef

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
 
+import { useState, useEffect } from 'react';
+import AccordionItem from './components/AccordionItem';
+import items from './data/AccordionItems';
 import './App.css';
 
-class App extends React.Component {
+const App = () => {
+  const [accordionItems, setAccordionItems] = useState([]);
 
-  render() { 
-    return (
-      <div className="App">
-        <h2>FAQ</h2>
-      </div>
-    );
+  useEffect(() => {
+    getAccordionItems();
+  }, []);
+
+  const getAccordionItems = () => {
+    Promise.resolve(items)
+      .then(data => setAccordionItems(data));
   }
+
+  return (
+    <div className="App">
+      <h2>FAQ</h2>
+      {
+        accordionItems.map((e, c) => <AccordionItem key={`ai${c}`} title={e.title} content={e.content} />)
+      }
+    </div>
+  );
 }
 
 export default App;
--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,23 +1,36 @@
-import React from "react";
+
+import { useState, useRef } from "react";
 import Chevron from "./Chevron";
 
 import "./Accordion.css";
 
-class AccordionItem extends React.Component {
+const AccordionItem = (props) => {
+    const [active, setActive] = useState(false);
+    const [height, setHeight] = useState('0px');
 
-    render() {
-        return (
-            <div className="accordion__section">
-                <button className="accordion">
-                    <p className="accordion__title"></p>
-                    <Chevron className="accordion__icon" width={10} fill={"#777"} />
-                </button>
-                <div className="accordion__content">
-                    <div className="accordion__text"></div>
-                </div>
-            </div>
-        );
+    const contentElement = useRef();
+
+    const toggleAccordion = () => {
+        updateAccordionView(!active);
     }
+
+    const updateAccordionView = (active) => {
+        const height = !active ? '0px' : `${contentElement.current.scrollHeight}px`;
+        setActive(active);
+        setHeight(height);
+    }
+   
+    return (
+        <div className="accordion__section">
+            <button className={`accordion${active ? ' active' : ''}`} onClick={() => toggleAccordion()}>
+                <p className="accordion__title">{props.title}</p>
+                <Chevron className={`accordion__icon${active ? ' rotate' : ''}`} width={10} fill={"#777"} />
+            </button>
+            <div className="accordion__content" ref={contentElement} style={{ maxHeight: `${height}`}}>
+                <div className="accordion__text">{props.content}</div>
+            </div>
+        </div>
+    );
 }
 
 export default AccordionItem;
