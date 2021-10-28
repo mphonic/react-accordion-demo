f62cf68a022aeb01ef3215aef5b1b0a55d74e5ac
Bhob Rainey <lloydlyson@gmail.com>
2021-10-28 12:41:27 -0400

    Convert App component to function

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@ import items from './data/AccordionItems';
 
 import './App.css';
 
-class App extends React.Component {
-
-  render() { 
-    return (
-      <div className="App">
-        <h2>FAQ</h2>
-        {
-          items.map((e, c) => (
-            <AccordionItem
-              key={`item${c}`}
-              title={e.title}
-              detail={e.content}
-            />
-          ))
-        }
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <h2>FAQ</h2>
+      {
+        items.map((e, c) => (
+          <AccordionItem
+            key={`item${c}`}
+            title={e.title}
+            detail={e.content}
+          />
+        ))
+      }
+    </div>
+  );
 }
 
 export default App;
