2a18edb0a477839474fa2ae68c7b24582e3dcd3b
Bhob Rainey <lloydlyson@gmail.com>
2021-10-28 11:09:33 -0400

    Tech-div presentation 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import AccordionItem from './components/AccordionItem';
+import items from './data/AccordionItems';
 
 import './App.css';
 
@@ -8,6 +10,15 @@ class App extends React.Component {
     return (
       <div className="App">
         <h2>FAQ</h2>
+        {
+          items.map((e, c) => (
+            <AccordionItem
+              key={`item${c}`}
+              title={e.title}
+              detail={e.content}
+            />
+          ))
+        }
       </div>
     );
   }
--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,23 +1,31 @@
-import React from "react";
+import { useState, useRef } from "react";
 import Chevron from "./Chevron";
 
 import "./Accordion.css";
 
-class AccordionItem extends React.Component {
+const AccordionItem = ({ title, detail }) => {   
+    const [active, setActive] = useState(false);
+    const [height, setHeight] = useState('0px');
 
-    render() {
-        return (
-            <div className="accordion__section">
-                <button className="accordion">
-                    <p className="accordion__title"></p>
-                    <Chevron className="accordion__icon" width={10} fill={"#777"} />
-                </button>
-                <div className="accordion__content">
-                    <div className="accordion__text"></div>
-                </div>
-            </div>
-        );
+    const contentElement = useRef();
+
+    const toggleAccordion = () => {
+        const localHeight = active ? '0px' : `${contentElement.current.scrollHeight}px`;
+        setActive(!active);
+        setHeight(localHeight);
     }
+
+    return (
+        <div className="accordion__section">
+            <button className={`accordion${active ? ' active' : ''}`} onClick={() => { toggleAccordion() }}>
+                <p className="accordion__title">{title}</p>
+                <Chevron className={`accordion__icon${active ? ' rotate' : ''}`} width={10} fill={"#777"} />
+            </button>
+            <div ref={contentElement} className="accordion__content" style={{ maxHeight: `${height}` }}>
+                <div className="accordion__text" dangerouslySetInnerHTML={{ __html: detail }}></div>
+            </div>
+        </div>
+    );
 }
 
 export default AccordionItem;
